261daaa2c8ca3e5c3a830f5c560a66dc5f36fb1a
Flavie Laurens <flavilo@hotmail.fr>
2019-03-29 13:45:11 +0100

    test (resource): fix

--- src/app/services/mls.service.spec.ts
+++ src/app/services/mls.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MlsService } from './mls.service';
+import { AppInitService } from '../app-init.service';
 
 describe('MlsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      AppInitService
+    ]
+  }));
 
   it('should be created', () => {
     const service: MlsService = TestBed.get(MlsService);
